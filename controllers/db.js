1e347c0ef8e166d675060031e3564affab969b3a
HakeemDAbdulmalik <hakeem.d.abdulmalik@gmail.com>
2022-06-18 13:32:06 +0000

    Updated Host db

--- controllers/db.js
+++ controllers/db.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize('myProducts', 'hakeem', 'student', {
-	host: '54.235.4.33',
+	host: '34.229.172.226',
 	port: 5432,
   dialect: 'postgres'
 });
